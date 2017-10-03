75a9181714911fedb523e2f0bddeed2c2eb744c0
João <joao.yuki@gmail.com>
2017-10-03 14:16:41 -0300

    Adicionado classe que vai fazer todo o log de chamadas REST

---
+++ src/main/java/com/controleFinanceiro/config/LoggingConfig.java
@@ -0,0 +1,21 @@
+package com.controleFinanceiro.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class LoggingConfig {
+	
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+	    CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+	    loggingFilter.setIncludeClientInfo(true);
+	    loggingFilter.setIncludeQueryString(true);
+	    loggingFilter.setIncludePayload(true);
+	    loggingFilter.setIncludeClientInfo(true);
+	    loggingFilter.setIncludeQueryString(true);
+	    return loggingFilter;
+	}
+
+}
